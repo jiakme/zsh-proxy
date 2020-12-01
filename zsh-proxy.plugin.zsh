9e5e03b03bd245efe769dc825cc61b637f59c242
jiakme <jiakme@163.com>
2020-12-01 11:06:26 +0800

    remove ip check

--- zsh-proxy.plugin.zsh
+++ zsh-proxy.plugin.zsh
@@ -296,13 +296,13 @@ config_proxy() {
 proxy() {
 	echo "1" >"${HOME}/.zsh-proxy/status"
 	__enable_proxy
-	__check_ip
+	# __check_ip
 }
 
 noproxy() {
 	echo "0" >"${HOME}/.zsh-proxy/status"
 	__disable_proxy
-	__check_ip
+	# __check_ip
 }
 
 myip() {
